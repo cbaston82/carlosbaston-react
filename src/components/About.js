c804607d464fe29f70c36440f3d1c2ee50529340
Carlos Baston <Carlos.Baston@people10.com>
2019-05-13 21:19:54 -0700

    remove knowledge from about me

--- src/components/About.js
+++ src/components/About.js
@@ -25,120 +25,115 @@ const About = () => (
             Developer with a little bit of PHP on the side. Outside of working
             and studying to perfect my development skills, I also enjoys playing
             the game of billiards (9-Ball and 8-ball to be exact) around the Las
-            Vegas area. I&rsquo;m currently employed and not accepting any full-time
-            work, however I&rsquo;am open on working on side projects.
-            {' '}
-            Feel free to
-            contact me if you have a new or current project you would like for
-            me to look at or to view a list of my personal projects.
+            Vegas area.
           </p>
         </div>
       </div>
-      <div className="row clearfix">
+      {/* <div className="row clearfix">
         <div className="col-md-12 text-center">
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/vue-logo.png" alt="" title="" />
             {' '}
             VUE.JS
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/sass-logo.png" alt="" title="" />
             {' '}
             SASS
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/rethinkdb-logo.png" alt="" title="" />
             {' '}
             RETHINKDB
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             {' '}
             <img src="assets/img/logos/php-logo.jpg" alt="" title="" />
             {' '}
             PHP
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/nodejs-logo.png" alt="" title="" />
             {' '}
             NODE.JS
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/mongodb-logo.png" alt="" title="" />
             {' '}
             MONGODB
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/json-logo.png" alt="" title="" />
             {' '}
             JSON
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/jquery-logo.png" alt="" title="" />
             {' '}
             JQUERY
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/javascript-logo.png" alt="" title="" />
             {' '}
             JAVASCRIPT
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/html-logo.png" alt="" title="" />
             {' '}
             HTML
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/grunt-logo.png" alt="" title="" />
             {' '}
             GRUNT
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/css-logo.png" alt="" title="" />
             {' '}
             CSS
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/bower-logo.png" alt="" title="" />
             {' '}
             BOWER
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/angular-logo.png" alt="" title="" />
             {' '}
             ANGULAR
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/ajax-logo.png" alt="" title="" />
             {' '}
             AJAX
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/laravel-logo.png" alt="" title="" />
             {' '}
             LARAVEL
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/npm-logo.png" alt="" title="" />
             {' '}
             NPM
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/composer-logo.png" alt="" title="" />
             {' '}
             COMPOSER
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/react-logo.png" alt="" title="" />
             {' '}
             REACT
           </p>
-          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6 knowledge">
+          <p className="col-lg-2 col-md-3 col-sm-4 col-xs-6">
             <img src="assets/img/logos/webpack-logo.png" alt="" title="" />
             {' '}
             WEBPACK
           </p>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </div>
   </section>
 );
